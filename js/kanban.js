7f7b955eb684dfa3aa23840b56890d0db211bc4c
snychka <stefannychka@gmail.com>
2019-08-12 16:58:29 -0600

    try first task, along with "compilation"

--- js/kanban.js
+++ js/kanban.js
@@ -7,6 +7,7 @@ const message = 'Please add a description.';
 const add_btn = document.querySelector('.add');
 add_btn.addEventListener('click', () => {
   const target = document.querySelector('#requested');
+  console.log(target);
   if (adding == false) {
     adding = true;
     target.appendChild(create_item());
@@ -16,6 +17,8 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  const item = document.createElement('div');
+  return item; // Stef, added for now to get to "compile"
 };
 
 document.querySelectorAll('.drop').forEach(element => {
